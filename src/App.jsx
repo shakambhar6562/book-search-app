e93dc57b29385c772b281ba01b644b0674b167f2
shakambhar <shakambhar6562>
2024-02-18 01:36:46 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,23 @@ import { BOOKS_API } from "./routingApi";
 import { RetrieveBookInfo } from "./utils/retrieveBookInfo";
 import { BookArrivals } from "./components/BookArrivals";
 import { useEffect } from "react";
+import { ColorRing } from "react-loader-spinner";
 
 export const App = () => {
   const [searchValue, setSearchValue] = useState("");
   const [bookData, setBookData] = useState([]);
   const [isBookDataLoading, setBookDataLoading] = useState(false);
+  const [isSiteLoading, setSiteLoading] = useState(true);
   const url = BOOKS_API + `=${searchValue || "finance"}`;
   const isValueChanged = useDeferredValue(searchValue);
   useEffect(() => {
     handleSearch();
+  }, [isValueChanged]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSiteLoading(false);
+    }, 1500);
   }, [isValueChanged]);
 
   const handleSearch = async () => {
@@ -30,7 +38,13 @@ export const App = () => {
     handleSearch,
     isBookDataLoading,
   };
-
+  if (isSiteLoading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <ColorRing visible={true} height="80" width="80" ariaLabel="color-ring-loading" wrapperStyle={{}} wrapperClass="color-ring-wrapper" colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]} />
+      </div>
+    );
+  }
   return (
     <>
       <Header {...headerProps} />
